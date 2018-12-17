be01c0fde9f87d62957cb92ba03dd1c228db23e1
ph1d0w <pablosntsmrtn4@gmail.com>
2018-12-17 19:42:51 +0100

    Cambios

--- js/basic.js
+++ js/basic.js
@@ -8,3 +8,28 @@ $(document).ready(function()
 
     $("#notificacion").delay(5000).fadeOut("slow");
 });
+
+function tiposProblema()
+{
+    var x = document.getElementById("selectTipo").value;
+    var tipo = parseInt(x);
+    switch(tipo)
+    {
+        case 1:
+            $('#dolores').css({
+                'display' : 'block'
+            });
+            $('#inflamaciones').css({
+                'display' : 'none'
+            });
+            break;
+        case 2:
+            $('#inflamaciones').css({
+                'display' : 'block'
+            });
+            $('#dolores').css({
+                'display' : 'none'
+            });
+            break;
+    }
+}
